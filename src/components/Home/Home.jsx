6e66b3e64fa907d001cac013ccd9574ccb38ba92
Mr. Md. Nazrul Islam <57587377+engrnazrulislam@users.noreply.github.com>
2024-06-10 00:42:20 +0600

    first commit

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,9 @@
+const Home = () => {
+    return (
+        <div style={{textAlign:"center", padding:"20px"}}>
+            <h2>Welcome to my Home</h2>
+        </div>
+    );
+};
+
+export default Home;
